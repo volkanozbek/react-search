c7531c57ca9b457692a152a7fd505d3063cf367e
Volkan Özbek <volkanozbk@hotmail.com>
2022-11-29 11:29:10 +0300

    learn

--- src/App.js
+++ src/App.js
@@ -7,14 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>Hey, vooz is here</code>
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >Test</a>
+        >
+          Test
+        </a>
       </header>
     </div>
   );
