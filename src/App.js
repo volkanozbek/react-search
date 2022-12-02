727458c67aefb3583f2ce1f4aa813ae1692959a4
Volkan Özbek <volkanozbk@hotmail.com>
2022-12-02 09:24:05 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Component } from "react";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => console.log(response))
+      .then((users) => console.log(users));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            <code>Hey, vooz is here</code>
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Test
-          </a>
-        </header>
+        {this.state.monsters.map((monster) => {
+          return <div>{monster.name}</div>;
+        })}
       </div>
     );
   }
